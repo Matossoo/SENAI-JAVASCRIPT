1c1fb2867ae3fe6813f1c3e6542b57c85b841748
Rafael Fenilli Matosso <rafael_matosso@estudante.sesisenai.org.br>
2026-07-13 08:57:30 -0300

    Update script.js

--- ESTRUTURA_API/frontend/script.js
+++ ESTRUTURA_API/frontend/script.js
@@ -4,15 +4,56 @@ async function ListarCurso(){
 
     const lista = document.getElementById('lista');
     lista.innerHTML = '';
-
+    
     cursos.forEach(curso => {
         lista.innerHTML += `
-        <li>${curso.id} - ${curso.nome}</li>
         
+        <li> 
+        ${curso.id} - ${curso.nome}
+            <button onclick="editarCurso(${curso.id})", '${curso.nome}')"> Editar</button>
+            <button onclick="excluirCurso(${curso.id})">Excluir</button>
+        </li>
         `
-
+    
 
 
     });
 }
 
+async function CadastrarCurso() {
+    const nome = document.getElementById('nome').value;
+
+    if(nome === '') {
+     alert('Por favor, preencha o nome do curso.');
+     return;
+    }
+
+    const resposta = await fetch('http://localhost:3023/cursos', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ nome })
+
+    })
+
+    const dados = await resposta.json();
+    alert(dados.mensagem);
+
+    document.getElementById('nome').value = '';
+    ListarCurso();
+}
+
+async function editarCurso() {
+
+    listarCurso();
+
+}
+
+async function excluirCurso(id) {
+    if(!confirm('Deseja realmente excluir este curso?')) return;
+    
+    await fetch(`http://localhost:3023/cursos/${id}`, {
+        method: 'DELETE'
+    })
+
+    ListarCurso();
+}
